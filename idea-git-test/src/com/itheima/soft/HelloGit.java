7d5f19f199f67632d67e817599e14f52d32801d4
Ben <ben@foxmail.com>
2019-06-08 19:20:19 +0800

    添加test的工程到git中

---
+++ idea-git-test/src/com/itheima/soft/HelloGit.java
@@ -0,0 +1,18 @@
+package com.itheima.soft;
+
+/**
+ * @ClassName:
+ * @Description:
+ * @Author: benliang
+ * @Date: 2019/6/8
+ * @Time: 19:18
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
